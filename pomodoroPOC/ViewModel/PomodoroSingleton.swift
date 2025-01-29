9e4615b77a1e2a4a81306d5322e6671cdbb6ab06
Caio Pessoa <caiopessoa1298@gmail.com>
2025-01-29 15:59:17 -0300

    fix bug on finishPomodoro play

--- pomodoroPOC/ViewModel/PomodoroSingleton.swift
+++ pomodoroPOC/ViewModel/PomodoroSingleton.swift
@@ -14,20 +14,20 @@ class PomodoroSingleton {
     private var initialClockCentiSeconds: Int?
     private var initialRestTimeCentiSeconds: Int?
     private var clockCentiSeconds: Int?
-    private var TrackClock: ((Int, Int, Bool) -> Void)? // Dá pra colocar em uma interface
+    private var TrackClock: ((Int, Int, Bool, Bool) -> Void)? // Dá pra colocar em uma interface
     var recover: Bool = false
     
     private init() {}
     static let shared = PomodoroSingleton()
     
     
-    func initialConfig(_ pomodoro: Pomodoro, updateClock: ((Int, Int, Bool) -> Void)? = nil) { // Ajeitar a lógica de receber em segundos
+    func initialConfig(_ pomodoro: Pomodoro, updateClock: ((Int, Int, Bool, Bool) -> Void)? = nil) { // Ajeitar a lógica de receber em segundos
         
         clockCentiSeconds = pomodoro.workTime * 100
         self.initialClockCentiSeconds = pomodoro.workTime * 100
         self.initialRestTimeCentiSeconds = pomodoro.restTime * 100
         self.TrackClock = updateClock
-
+        
     }
     
     func play() {
@@ -36,12 +36,12 @@ class PomodoroSingleton {
         isRunning = true
         
         timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { [weak self] _ in
-
+            
             guard let self = self else { return }
             guard  self.clockCentiSeconds != nil else { return }
             
             let isClockOver = self.clockCentiSeconds == 0
-
+            
             if isClockOver {
                 recover = true
                 pauseClock()
@@ -49,15 +49,15 @@ class PomodoroSingleton {
                 playRecover()
                 return
             }
-
+            
             self.clockCentiSeconds! -= 1
             
             if let TrackClock = self.TrackClock {
-
+                
                 let clockSeconds = Double(self.clockCentiSeconds!)/100
                 let clockSecondsCeil = Int(ceil(clockSeconds))
-
-                TrackClock(clockSecondsCeil, self.clockCentiSeconds!, recover)
+                
+                TrackClock(clockSecondsCeil, self.clockCentiSeconds!, recover, isRunning)
             }
         }
     }
@@ -70,34 +70,34 @@ class PomodoroSingleton {
         isRunning = true
         
         timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { [weak self] _ in
-
+            
             guard let self = self else { return }
             guard  self.clockCentiSeconds != nil else { return }
             
             let isClockOver = self.clockCentiSeconds == 0
-
+            
             if isClockOver {
                 recover = false
                 pauseClock()
                 resetClock()
                 if let TrackClock = self.TrackClock {
-
+                    
                     let clockSeconds = Double(self.clockCentiSeconds!)/100
                     let clockSecondsCeil = Int(ceil(clockSeconds))
-
-                    TrackClock(clockSecondsCeil, self.clockCentiSeconds!, recover)
+                    
+                    TrackClock(clockSecondsCeil, self.clockCentiSeconds!, recover, isRunning)
                 }
                 return
             }
-
+            
             self.clockCentiSeconds! -= 1
             
             if let TrackClock = self.TrackClock {
-
+                
                 let clockSeconds = Double(self.clockCentiSeconds!)/100
                 let clockSecondsCeil = Int(ceil(clockSeconds))
-
-                TrackClock(clockSecondsCeil, self.clockCentiSeconds!, recover)
+                
+                TrackClock(clockSecondsCeil, self.clockCentiSeconds!, recover, isRunning)
             }
         }
     }
@@ -106,7 +106,6 @@ class PomodoroSingleton {
         guard isRunning else { return }
         isRunning = false
         print("PAUSE \(isRunning)")
-//        updateIsRunning(isRunning)
         
         timer?.invalidate()
         timer = nil
